e3faca4fb9d996720b1ddce6af6005100bc6f2c5
yumin-jung <jungym887@naver.com>
2025-07-15 13:26:27 +0900

    Initial Commit

---
+++ TabBariOS26/TabBariOS26App.swift
@@ -0,0 +1,17 @@
+//
+//  TabBariOS26App.swift
+//  TabBariOS26
+//
+//  Created by yumin on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct TabBariOS26App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
